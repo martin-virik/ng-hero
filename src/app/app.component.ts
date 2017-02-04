fe087ff54523c96b21c332638b39575ca83d3653
Martin Virik <MVirik@sk.ibm.com>
2017-02-04 20:58:24 +0100

    Routing III

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
