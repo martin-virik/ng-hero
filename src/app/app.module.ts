1194b6bfccf3b9167dc1480db3e402b7f87782fb
Martin Virik <MVirik@sk.ibm.com>
2017-02-04 23:25:33 +0100

    HTTP III

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard.component';
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService }         from './hero.service';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule }    from './app-routing.module';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { AppRoutingModule }     from './app-routing.module';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
